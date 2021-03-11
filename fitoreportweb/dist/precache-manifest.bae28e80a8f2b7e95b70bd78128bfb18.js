1d5f1bb34937c93139660b678e7d66d8ff63bb1c
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2021-03-10 23:45:06 -0600

    Merge

---
+++ fitoreportweb/dist/precache-manifest.bae28e80a8f2b7e95b70bd78128bfb18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8f82b16b00ec697951",
+    "url": "/css/app.06dbd0df.css"
+  },
+  {
+    "revision": "2f766f0e5266812e464a",
+    "url": "/css/chunk-vendors.9f4762b9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3d06b3d25e8f07a773f2937965a6766d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf8f82b16b00ec697951",
+    "url": "/js/app.18255d1a.js"
+  },
+  {
+    "revision": "730d618dee828529266e",
+    "url": "/js/chunk-2d0d3daa.5e047ac6.js"
+  },
+  {
+    "revision": "590019ff541fb0155fa9",
+    "url": "/js/chunk-537143fc.82b34347.js"
+  },
+  {
+    "revision": "2f766f0e5266812e464a",
+    "url": "/js/chunk-vendors.133bbcaf.js"
+  },
+  {
+    "revision": "4074dd57a1d48dc7a5ba6abb7bed64b5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
