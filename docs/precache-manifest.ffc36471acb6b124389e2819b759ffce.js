2e3f43c82e7176d4bb25d08f6f7ab96462f21be0
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2021-03-10 18:05:11 -0600

    Deploy test 2

---
+++ docs/precache-manifest.ffc36471acb6b124389e2819b759ffce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227354763edcf4caadde",
+    "url": "/css/app.06dbd0df.css"
+  },
+  {
+    "revision": "2f766f0e5266812e464a",
+    "url": "/css/chunk-vendors.9f4762b9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fd1808e4d2c6f72a2c994b4dab882040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "227354763edcf4caadde",
+    "url": "/js/app.972fd386.js"
+  },
+  {
+    "revision": "339439a64fc7559226c3",
+    "url": "/js/chunk-2d0d3daa.e040dd3b.js"
+  },
+  {
+    "revision": "67d022e6c79eb2bf8fb3",
+    "url": "/js/chunk-537143fc.62b256fe.js"
+  },
+  {
+    "revision": "2f766f0e5266812e464a",
+    "url": "/js/chunk-vendors.133bbcaf.js"
+  },
+  {
+    "revision": "4074dd57a1d48dc7a5ba6abb7bed64b5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
